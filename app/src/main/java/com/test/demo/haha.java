dcea4ecad882f0d49af0bca5712a72157850f4af
HaDog <android_wsm@163.com>
2019-12-25 16:38:16 +0800

    intiView

--- app/src/main/java/com/test/demo/haha.java
+++ app/src/main/java/com/test/demo/haha.java
@@ -1,14 +1,23 @@
 package com.test.demo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 2019 重庆指讯科技股份有限公司
+ *
+ * @author: hkc #3
+ * @date: 2019/12/25 13:06
+ * @description:
+ */
 public class haha extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_haha);
+        setContentView(R.layout.activity_main);
+        finish();
     }
 }
