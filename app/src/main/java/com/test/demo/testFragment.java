b389c6ae07a2fc0a46cad6b788d759c3827aa107
HaDog <android_wsm@163.com>
2019-12-25 17:26:15 +0800

    增加文件大小判單

---
+++ app/src/main/java/com/test/demo/testFragment.java
@@ -0,0 +1,13 @@
+package com.test.demo;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * 2019 重庆指讯科技股份有限公司
+ *
+ * @author: hkc #3
+ * @date: 2019/12/25 17:25
+ * @description:
+ */
+public class testFragment extends Fragment {
+}
