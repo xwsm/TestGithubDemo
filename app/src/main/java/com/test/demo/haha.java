9f4bec0a9cbe9716f7e2d00e74661e11ac82b2de
HaDog <android_wsm@163.com>
2019-12-25 13:08:34 +0800

    增加布局

---
+++ app/src/main/java/com/test/demo/haha.java
@@ -0,0 +1,23 @@
+package com.test.demo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 2019 重庆指讯科技股份有限公司
+ *
+ * @author: hkc #3
+ * @date: 2019/12/25 13:06
+ * @description:
+ */
+public class haha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        finish();
+    }
+}
