4509ad3095fd1031c577ef7693de84e1ce1295ce
HaDog <android_wsm@163.com>
2019-12-30 13:30:58 +0800

    測試1

--- app/src/main/java/com/test/demo/haha.java
+++ app/src/main/java/com/test/demo/haha.java
@@ -17,7 +17,8 @@ public class haha extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.haha);
         finish();
+
     }
 }
